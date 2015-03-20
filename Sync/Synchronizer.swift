5633637c32cf371b76744cad2f75bb38da4983f8
Richard Newman <rnewman@twinql.com>
2015-03-20 15:20:11 -0700

    Rebase after sharing Prefs.

--- Sync/Synchronizer.swift
+++ Sync/Synchronizer.swift
@@ -3,21 +3,22 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
+import Shared
 
 // TODO: return values?
 public protocol Synchronizer {
-    init(info: InfoCollections, prefs: ProfilePrefs)
+    init(info: InfoCollections, prefs: Prefs)
     func synchronize()
 }
 
 public class ClientsSynchronizer: Synchronizer {
     private let info: InfoCollections
-    private let prefs: ProfilePrefs
+    private let prefs: Prefs
 
     private let prefix = "clients"
     private let collection = "clients"
 
-    required public init(info: InfoCollections, prefs: ProfilePrefs) {
+    required public init(info: InfoCollections, prefs: Prefs) {
         self.info = info
         self.prefs = prefs
     }
