7712d19f5eef5e0d8bdb3280e68eac6cc9e3c7c0
sachin <sachin.irukula@gmail.com>
2015-05-07 21:59:58 -0500

    Small fixes and relative date comment changes

--- Utils/TimeConstants.swift
+++ Utils/TimeConstants.swift
@@ -28,29 +28,37 @@ extension NSDate {
         let formatter = NSDateFormatter()
         
         if components.year > 0 {
-            return String(format: NSLocalizedString("%@", comment: "relative time"), NSDateFormatter.localizedStringFromDate(self, dateStyle: NSDateFormatterStyle.ShortStyle, timeStyle: NSDateFormatterStyle.ShortStyle))
-        } else if components.month > 0 {
+            return String(format: NSLocalizedString("%@", comment: "Relative date for date older than an year."), NSDateFormatter.localizedStringFromDate(self, dateStyle: NSDateFormatterStyle.ShortStyle, timeStyle: NSDateFormatterStyle.ShortStyle))
+        }
+
+        if components.month > 0 {
             if components.month == 1 {
-                return String(format: NSLocalizedString("More than a month ago", comment: "relative time"))
+                return String(format: NSLocalizedString("More than a month ago", comment: "Relative date for dates older than a month."))
             }
             else
             {
-                return String(format: NSLocalizedString("%@", comment: "relative time"), NSDateFormatter.localizedStringFromDate(self, dateStyle: NSDateFormatterStyle.ShortStyle, timeStyle: NSDateFormatterStyle.ShortStyle))
+                return String(format: NSLocalizedString("%@", comment: "Relative date for date older than two months."), NSDateFormatter.localizedStringFromDate(self, dateStyle: NSDateFormatterStyle.ShortStyle, timeStyle: NSDateFormatterStyle.ShortStyle))
             }
-        } else if components.weekOfYear > 0 {
-            return String(format: NSLocalizedString("More than a week ago", comment: "relative time"))
-        } else if components.day > 0 {
-            if components.day > 1 {
-                return String(format: NSLocalizedString("This week", comment: "relative time"), String(components.day))
-            } else if components.day == 1 {
-                return String(format: NSLocalizedString("Yesterday", comment: "relative time"))
-            }
-        } else if components.hour > 0 || components.minute > 0{
-            formatter.timeStyle = .ShortStyle
-            return String(format: NSLocalizedString("Today at %@", comment: "relative time"), formatter.stringFromDate(self))
         }
 
-        return String(format: NSLocalizedString("Just now", comment: "relative time"))
+        if components.weekOfYear > 0 {
+            return String(format: NSLocalizedString("More than a week ago", comment: "Relative date for date older than a week."))
+        }
+
+        if components.day > 0 {
+            if components.day > 1 {
+                return String(format: NSLocalizedString("This week", comment: "Relative date for date older than two days."), String(components.day))
+            } else if components.day == 1 {
+                return String(format: NSLocalizedString("Yesterday", comment: "Relative date for date older than a day and newer than two days."))
+            }
+        }
+
+        if components.hour > 0 || components.minute > 0{
+            formatter.timeStyle = .ShortStyle
+            return String(format: NSLocalizedString("Today at %@", comment: "Relative date for date older than a minute."), formatter.stringFromDate(self))
+        }
+
+        return String(format: NSLocalizedString("Just now", comment: "Relative date for current time."))
     }
 }
 
@@ -68,6 +76,6 @@ public func millisecondsToDecimalSeconds(input: Timestamp) -> String {
 
 extension Timestamp {
     public func toNSDate() -> NSDate {
-        return NSDate(timeIntervalSince1970: NSTimeInterval(self / 1000))
+        return NSDate(timeIntervalSince1970: NSTimeInterval(millisecondsToDecimalSeconds(self).toInt()!))
     }
 }
